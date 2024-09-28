a8dd898ee5165f910979bfae3e3a50c6029f33df
Haryutake <th1425y.beagm@gmail.com>
2024-09-28 14:58:22 +0900

    課題007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+
+<?php
+// 配列
+$date = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+// 連想配列を出力
+print_r($date);
+?>
+
+</p>
+ </body>
+ 
+ </html>
